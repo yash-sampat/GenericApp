3c87229981b0ccc56cd27047d12ef93f04f9a2e3
yash-sampat <yash.sampat@protonmail.com>
2024-12-16 20:39:46 +0530

    fix - restore list scroll position on back nav (attempt to restore or prevent fragment re-creation).

--- app/src/main/java/com/generic/login/view/home/HomeFragment.kt
+++ app/src/main/java/com/generic/login/view/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.generic.login.view.home
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.generic.login.adapter.LoadStateAdapter
 import com.generic.login.adapter.ProductAdapter
 import com.generic.login.adapter.ProductComparator
@@ -21,31 +23,57 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     override val viewModel: HomeViewModel by viewModels()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        showInfiniteScrollingList()
-    }
+    private val listState: String = "listState"
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ) = FragmentHomeBinding.inflate(inflater, container, false)
 
-    private fun showInfiniteScrollingList() = with(binding){
-        val pagingAdapter = ProductAdapter(ProductComparator)
-        recyclerView.adapter = pagingAdapter
-        recyclerView.setHasFixedSize(true)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        retainInstance = true
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
-        lifecycleScope.launch {
-            viewModel.getPhotosPaged().collectLatest { pagingData ->
-                pagingAdapter.submitData(pagingData)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showInfiniteScrollingList()
+    }
+
+    private fun showInfiniteScrollingList() = with(binding){
+        if(recyclerView.adapter == null) {
+            val pagingAdapter = ProductAdapter(ProductComparator)
+            pagingAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            recyclerView.adapter =
+                pagingAdapter.withLoadStateHeaderAndFooter(
+                    header = LoadStateAdapter { pagingAdapter.retry() },
+                    footer = LoadStateAdapter { pagingAdapter.retry() }
+                )
+            recyclerView.setHasFixedSize(true)
+            lifecycleScope.launch {
+                viewModel.getPhotosPaged().collectLatest { pagingData ->
+                    pagingAdapter.submitData(pagingData)
+                }
             }
         }
-        recyclerView.adapter =
-            pagingAdapter.withLoadStateHeaderAndFooter(
-                header = LoadStateAdapter { pagingAdapter.retry() },
-                footer = LoadStateAdapter { pagingAdapter.retry() }
-            )
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val mListState: Parcelable? = binding.recyclerView.layoutManager?.onSaveInstanceState()
+        outState.putParcelable(listState, mListState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        if (savedInstanceState != null) {
+            binding.recyclerView.layoutManager?.onRestoreInstanceState(savedInstanceState.getBundle(listState))
+        }
     }
 }
